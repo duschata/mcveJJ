8b6ed2c958f9c104ccffd97ba3fd0351b110d11f
Thomas Grabietz <grabietz@gipmbh.de>
2024-10-14 07:11:03 +0200

    copy/rename bulk

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,3 +75,11 @@ tasks.register<TaskWithInputList>("taskWithInputList") {
 tasks.register<TaskWithConfigurableFileCollection>("taskWithConfigurableFileCollection") {
    inputDirectories.from(taskWithOutput.get().output)
 }
+
+tasks.register<CopyBulkTask>("copyBulkTask") {
+    fromFile = "project_tmpl"
+    intoFile = ".project"
+
+    directories = listOf(layout.projectDirectory.dir("other-source/projectA"), layout.projectDirectory.dir("other-source/projectB"))
+
+}
